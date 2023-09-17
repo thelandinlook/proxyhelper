b747e314ed3c461a238b172c380a70af08c88d48
Landin <76990913+thelandinlook@users.noreply.github.com>
2023-09-17 17:16:48 +0200

    Update FullScript-Windows.ps1
    Fixed not moving the proxy files if a proxy folder already existed.

--- FullScript-Windows.ps1
+++ FullScript-Windows.ps1
@@ -7,17 +7,19 @@ if (Test-Path $config_file_path) {
     $config = Get-Content $config_file_path
     $prev_drive = $config[0]
     Write-Host "Previous drive was: $prev_drive"
-    $drive = Read-Host "Would you like to use the previously selected SD card drive? (hit ENTER for yes or type a new drive)"
-    if ([string]::IsNullOrWhitespace($drive)) { 
+    $drive = Read-Host "Would you like to use the previous drive? (hit ENTER for yes or enter a new drive)"
+    if ([string]::IsNullOrWhitespace($drive)) {
         $drive = $prev_drive 
     }
 
     $prev_dir = $config[1]
     Write-Host "Previous directory was: $prev_dir"
     $dest_directory = Read-Host "Would you like to use the previous directory? (hit ENTER for yes or paste a new directory)"
-    if ([string]::IsNullOrWhitespace($dest_directory)) { $dest_directory = $prev_dir }
+    if ([string]::IsNullOrWhitespace($dest_directory)) {
+        $dest_directory = $prev_dir
+    }
 } else {
-    $drive = Read-Host "Enter the SD card's drive letter (without colon) containing the 'PRIVATE\M4ROOT' folders"
+    $drive = Read-Host "Enter the drive letter (without colon) containing the 'PRIVATE\M4ROOT' folders"
     $dest_directory = Read-Host "Enter the destination directory"
 }
 
@@ -37,27 +39,22 @@ foreach ($src_folder in $src_folders.GetEnumerator()) {
     $dest_folder = $dest_directory.TrimEnd('\') 
     if ($src_folder.Name -eq "SUB") {
         $dest_folder = Join-Path $dest_directory "Proxy"
-    }
-
-    if (-not (Test-Path $dest_folder)) { New-Item -ItemType Directory -Force -Path $dest_folder }
+        if (-not (Test-Path $dest_folder)) {
+            New-Item -ItemType Directory -Force -Path $dest_folder
+        }
+    } 
 
     try {
-        Get-ChildItem -Path $src_folder.Value |
-        ForEach-Object {
-            $new_name = $_.FullName
-            if ($src_folder.Name -eq "SUB") {
-                $_ | Where-Object { $_.Name -match 'S03' } |
-                ForEach-Object {
-                    $new_name = $_.Name.Replace('S03','')
-                    Rename-Item -Path $_.FullName -NewName $new_name
-                }
+        Get-ChildItem -Path $src_folder.Value | ForEach-Object {
+            $new_name = $_.BaseName
+            if ($_.Directory.Name -eq 'SUB') {
+                $new_name = $new_name -replace 'S03'
             }
-            
-            $dest_file = Join-Path $dest_folder $(Split-Path $new_name -Leaf)
+            $dest_file = Join-Path $dest_folder "$new_name$($_.Extension)"
             if (-not (Test-Path $dest_file)) {
                 Copy-Item -Path $_.FullName -Destination $dest_file -ErrorAction Stop
             }
-        }        
+        }  
         Write-Host "File copy operation was successful! $($src_folder.Value)"
     }
     catch {
